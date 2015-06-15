9d1617c76cd4f3889c54e110129fddfa2da453c7
dudap <dudap@ee.pw.edu.pl>
2015-06-15 14:37:28 +0200

    14:37-06-15 commit

--- PZP-forum/schema.sql
+++ PZP-forum/schema.sql
@@ -1,17 +1,17 @@
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
-  autor text not null,
+  autor varchar(20) not null,
   text text not null,
   time text not null
 );
 drop table if exists users;
 create table users (
   id integer primary key autoincrement,
-  autor text not null,
+  autor varchar(20) not null,
   password text not null, 
   mail text not null,
   token text not null,
-  change text not null
+  change varchar(3) not null
 );
 
