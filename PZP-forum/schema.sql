8016326c71a488c878857eb07145592beda3fb5a
dudap <dudap@ee.pw.edu.pl>
2015-06-15 14:32:21 +0200

    Revert "Second commit"
    This reverts commit e56a261b973ed1d43511af1b20f9dac12719314b.

--- PZP-forum/schema.sql
+++ PZP-forum/schema.sql
@@ -1,17 +1,17 @@
 drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
-  autor varchar(20) not null,
+  autor text not null,
   text text not null,
   time text not null
 );
 drop table if exists users;
 create table users (
   id integer primary key autoincrement,
-  autor varchar(20) not null,
+  autor text not null,
   password text not null, 
   mail text not null,
   token text not null,
-  change varchar() not null
+  change text not null
 );
 
